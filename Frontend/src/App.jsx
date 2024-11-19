5d91026c9f0f8fca23b71aa4b8c5e53cf13f11fe
Muhammad Yasin <m.yaseensalim@gmail.com>
2024-11-19 23:11:34 +0500

    footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import AboutUs from './Components/AboutUs'
 import Taskmanager from './Components/Taskmanager'
+import Footer from './Components/Footer'
 function App() {
   
   return (
@@ -11,6 +12,7 @@ function App() {
     <Hero/>
     <AboutUs/>
     <Taskmanager/>
+    <Footer/>
     </>
   )
 }
---
+++ Frontend/src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+import react from "react"
+import logo from "../assets/logo.svg"
+
+const Footer=()=>{
+    return(
+        <div className="footer">
+            <div>
+                <img src={logo} alt="logo"  height="100px" />
+                <h2>Developed By Muhammad Yasin</h2>
+            </div>
+            
+        </div>
+    )
+}
+export default Footer
